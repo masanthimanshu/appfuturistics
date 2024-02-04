7d5debe3f30860c7bd6365ef8c450a940afe83ce
himanshumasant <himanshu.masant@wisepromo.com>
2024-02-04 12:05:38 +0530

    updates

---
+++ src/components/home/OurServicesCard.jsx
@@ -0,0 +1,18 @@
+import style from "./style.module.css";
+import { Box, Typography, Grid } from "@mui/material";
+
+export const OurServicesCard = ({ icon, heading, description }) => {
+  return (
+    <Grid item md={4}>
+      <div className={style.services_card}>
+        <Typography variant="h3">{icon}</Typography>
+        <br />
+        <Typography variant="h4">
+          <b>{heading}</b>
+        </Typography>
+        <br />
+        <Typography>{description}</Typography>
+      </div>
+    </Grid>
+  );
+};
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,6 +1,8 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Typography, Grid } from "@mui/material";
+import { DesignServices, Code, AppShortcut } from "@mui/icons-material";
 import { BottomNavbar } from "../../components/navigation/BottomNavbar";
 import { HomeHeroSection } from "../../components/home/HomeHeroSection";
+import { OurServicesCard } from "../../components/home/OurServicesCard";
 
 export default function HomePage() {
   return (
@@ -8,20 +10,80 @@ export default function HomePage() {
       <HomeHeroSection />
       <BottomNavbar page="home" />
       <Box bgcolor="#fafafa" py={10}>
-        <Container sx={{ textAlign: "center" }}>
-          <Typography variant="h3">
+        <Container>
+          <Typography variant="h3" textAlign="center">
             <b>How can we help you?</b>
           </Typography>
           <br />
-          <Typography>
+          <Typography textAlign="center">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. In, ipsum
             quas laborum nostrum natus consequatur cum asperiores odio minus
             placeat quos atque sint minima qui pariatur soluta iste veritatis
             blanditiis?
           </Typography>
+          <br />
+          <br />
+          <br />
+          <Grid container spacing={5}>
+            <OurServicesCard
+              heading="Design"
+              icon={<DesignServices fontSize="inherit" />}
+              description="Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Obcaecati, quae doloremque! Aperiam sint sapiente excepturi
+                  quo, et vero fugit expedita ea adipisci molestias eligendi.
+                  Praesentium, molestias? Ab culpa facere modi."
+            />
+            <OurServicesCard
+              heading="Development"
+              icon={<Code fontSize="inherit" />}
+              description="Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Obcaecati, quae doloremque! Aperiam sint sapiente excepturi
+                  quo, et vero fugit expedita ea adipisci molestias eligendi.
+                  Praesentium, molestias? Ab culpa facere modi."
+            />
+            <OurServicesCard
+              heading="Marketing"
+              icon={<AppShortcut fontSize="inherit" />}
+              description="Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Obcaecati, quae doloremque! Aperiam sint sapiente excepturi
+                  quo, et vero fugit expedita ea adipisci molestias eligendi.
+                  Praesentium, molestias? Ab culpa facere modi."
+            />
+          </Grid>
         </Container>
       </Box>
-      <Container sx={{ height: "200vh" }}></Container>
+      <br />
+      <br />
+      <br />
+      <br />
+      <Container>
+        <Typography variant="h3" textAlign="center">
+          <b>Case Studies</b>
+        </Typography>
+        <br />
+        <Typography textAlign="center">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. In, ipsum
+          quas laborum nostrum natus consequatur cum asperiores odio minus
+          placeat quos atque sint minima qui pariatur soluta iste veritatis
+          blanditiis?
+        </Typography>
+        <br />
+        <br />
+        <Grid container spacing={2}>
+          <Grid item md={4}>
+            <img src="/desk.png" alt="Case Study" style={{ width: "100%" }} />
+          </Grid>
+          <Grid item md={4}>
+            <img src="/desk.png" alt="Case Study" style={{ width: "100%" }} />
+          </Grid>
+          <Grid item md={4}>
+            <img src="/desk.png" alt="Case Study" style={{ width: "100%" }} />
+          </Grid>
+        </Grid>
+        <br />
+        <br />
+      </Container>
+      <Box height={100}></Box>
     </>
   );
 }

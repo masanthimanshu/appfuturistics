40816b58f4469334188c65d15715559bf94afc03
himanshumasant <himanshu.masant@wisepromo.com>
2024-02-06 19:02:11 +0530

    updates

--- src/pages/services/ServicesPage.jsx
+++ src/pages/services/ServicesPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { Box } from "@mui/material";
+import { Box, Container, Grid, Typography } from "@mui/material";
 import style from "./style.module.css";
 import { ZoomSection } from "../../components/services/ZoomSection";
 import { BottomNavbar } from "../../components/navigation/BottomNavbar";
@@ -21,9 +21,69 @@ export default function ServicesPage() {
         </div>
       </Box>
       <ZoomSection />
-      <Box height={100}></Box>
+      <br />
+      <br />
+      <br />
+      <br />
+      <Container>
+        <Box border={2} borderRadius={2} p={4} borderColor="gray">
+          <Grid container spacing={5}>
+            <Grid item md={6}>
+              <Typography variant="h4">
+                <b>Mobile App</b>
+              </Typography>
+              <br />
+              <Typography>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit,
+                est dignissimos dolorem molestias iste porro nostrum! Eveniet
+                tempore ipsam pariatur ipsa, aliquid quod, nemo sequi, debitis
+                reprehenderit neque repellendus et!
+              </Typography>
+            </Grid>
+            <Grid item md={6}></Grid>
+          </Grid>
+        </Box>
+        <br />
+        <br />
+        <Box border={2} borderRadius={2} p={4} borderColor="gray">
+          <Grid container spacing={5}>
+            <Grid item md={6} order={2}>
+              <Typography variant="h4">
+                <b>Web App</b>
+              </Typography>
+              <br />
+              <Typography>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit,
+                est dignissimos dolorem molestias iste porro nostrum! Eveniet
+                tempore ipsam pariatur ipsa, aliquid quod, nemo sequi, debitis
+                reprehenderit neque repellendus et!
+              </Typography>
+            </Grid>
+            <Grid item md={6} order={1}></Grid>
+          </Grid>
+        </Box>
+        <br />
+        <br />
+        <Box border={2} borderRadius={2} p={4} borderColor="gray">
+          <Grid container spacing={5}>
+            <Grid item md={6}>
+              <Typography variant="h4">
+                <b>Mobile App</b>
+              </Typography>
+              <br />
+              <Typography>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit,
+                est dignissimos dolorem molestias iste porro nostrum! Eveniet
+                tempore ipsam pariatur ipsa, aliquid quod, nemo sequi, debitis
+                reprehenderit neque repellendus et!
+              </Typography>
+            </Grid>
+            <Grid item md={6}></Grid>
+          </Grid>
+        </Box>
+      </Container>
       <BottomNavbar page="services" />
-      <Box height={100}></Box>
+      <Box height={50}></Box>
     </>
   );
 }

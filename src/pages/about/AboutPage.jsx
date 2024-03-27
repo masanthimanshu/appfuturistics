7f5db6a8fd0d50f982034775cdb2a92f8c164df9
himanshumasant <himanshu.masant@wisepromo.com>
2024-03-27 17:52:35 +0530

    updates

--- src/pages/about/AboutPage.jsx
+++ src/pages/about/AboutPage.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
-import { Box } from "@mui/material";
 import style from "./style.module.css";
+import { Box, Grid, Container, Typography } from "@mui/material";
+import { ContactForm } from "../../components/contact/ContactForm";
 import { BottomNavbar } from "../../components/navigation/BottomNavbar";
 
 export default function AboutPage() {
@@ -19,12 +20,65 @@ export default function AboutPage() {
           <h2 id="subtitle">Code rules everything around us</h2>
         </div>
       </section>
-      <br />
-      <br />
       <BottomNavbar page="about" />
+      <Container>
+        <Grid container spacing={5}>
+          <Grid item md={6}>
+            <img src="/desk.png" alt="About Us" style={{ width: "100%" }} />
+          </Grid>
+          <Grid item md={6}>
+            <Typography variant="h4">What do we stand for?</Typography>
+            <br />
+            <Typography>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus
+              suscipit ex sit accusamus porro deserunt voluptates autem,
+              pariatur dolorum quae dolore beatae ipsam accusantium soluta quo
+              enim expedita vero eum?
+            </Typography>
+            <br />
+            <Typography>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus
+              suscipit ex sit accusamus porro deserunt voluptates autem,
+              pariatur dolorum quae dolore beatae ipsam accusantium soluta quo
+              enim expedita vero eum?
+            </Typography>
+          </Grid>
+        </Grid>
+      </Container>
       <br />
       <br />
-      <Box height={125}></Box>
+      <br />
+      <br />
+      <Container>
+        <Grid container spacing={5}>
+          <Grid item md={6}>
+            <Typography variant="h4">What do we stand for?</Typography>
+            <br />
+            <Typography>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus
+              suscipit ex sit accusamus porro deserunt voluptates autem,
+              pariatur dolorum quae dolore beatae ipsam accusantium soluta quo
+              enim expedita vero eum?
+            </Typography>
+            <br />
+            <Typography>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus
+              suscipit ex sit accusamus porro deserunt voluptates autem,
+              pariatur dolorum quae dolore beatae ipsam accusantium soluta quo
+              enim expedita vero eum?
+            </Typography>
+          </Grid>
+          <Grid item md={6}>
+            <img src="/desk.png" alt="About Us" style={{ width: "100%" }} />
+          </Grid>
+        </Grid>
+      </Container>
+      <br />
+      <br />
+      <br />
+      <br />
+      <ContactForm />
+      <Box height={50}></Box>
     </>
   );
 }
